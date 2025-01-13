cdc8bb1db6f6d17d034a66bfcac6f28c81620ba1
Gustavo Rezende <gustavo.alesalrez@outlook.com>
2025-01-13 20:29:48 -0300

    v.1.0.1

---
+++ src/styles/theme/default.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+      background: '#0d1117', 
+      text: '#c9d1d9', 
+      primary: '#238636', 
+      secondary: '#58a6ff', 
+      gradient: 'linear-gradient(90deg, #238636, #58a6ff)',
+    },
+    fonts: {
+      heading: "'Poppins', sans-serif",
+      body: "'Roboto', sans-serif",
+    },
+  };
+  
+  export default theme;
